4e5aff1923e1864e0c48f110161053a47c8e6dc5
siwka <ms.ksiwek@gmail.com>
2015-02-11 04:59:39 -0600

    had junk, overwrite almost all & tests in Array

--- lib/array.rb
+++ lib/array.rb
@@ -18,12 +18,75 @@ class Array
 
   def check_subsets_for_sum(prices, target)
     results = []
-    self.each do |a|
+    self.each do |a|  #check for arrays with different length - test
       tmp = a.zip(prices).map { |x,y| x*y } unless prices.nil?
       suma = tmp.reduce(:+) unless tmp.nil?
       results << a if suma == target
     end
     results
+  end
+
+  def match_prices_with_entrees(full_menu, prices)
+    # prices.to_money
+    sample_meals = []
+
+    # self.each do |qty|
+      # puts "to jest cos! #{qty}" - nie wchodzi nawet do petli
+
+# # qty = self[0] #siwka temp
+    for i in (0..self.length-1) do
+      qty = self[i]
+
+      selected_prices = qty.select_nonzero_indexes.select_result_prices(prices)
+      qty -= [0]
+      qty.validate_eq_length(selected_prices)
+      selected_prices.validate_eq_length(qty)
+      sample_meals += qty.combine_with(full_menu, selected_prices)
+    end
+    sample_meals
+# ---pierwsza wersja---
+    # qty = self
+    # selected_prices = self.select_nonzero_indexes.select_result_prices(prices)
+    
+    # qty.validate_eq_length(selected_prices)
+    # selected_prices.validate_eq_length(qty)
+
+    # qty.combine_with(full_menu, selected_prices)
+
+# ----------
+    # chyba nie uzyje?    qty.combine(selected_prices).match_with_entrees_from(full_menu)
+
+    # h.select{|k,v| v == "4.20"}.keys
+    # selection =   hash.key(value) => key
+  end
+
+  def to_money
+    self.map!{ |g| '%.2f' % (g.to_f/100) }
+  end
+
+  def select_nonzero_indexes
+    self.each_index.select{ |i| self[i] != 0 }
+  end
+
+  def select_result_prices(prices)
+    self.map{ |i| prices[i] } - [nil]
+  end
+
+  def combine(prices)
+    self.slice(0, prices.length).zip(prices)
+  end
+
+  def combine_with(full_menu, prices)
+    selected_entrees = []
+    prices.map do |a|
+      selected_entrees << full_menu.select{|k,v| v == a }.keys
+    end
+    self.zip(selected_entrees, prices)
+  end  
+
+  def validate_eq_length(arr)
+    self.slice!(0, arr.length) if self.length > arr.length
+    self
   end
 
   def odd_values
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,36 +2,87 @@ require 'array'
 
 describe Array do
 
-	let (:permutation) { [[7, 3, 7, 6, 5, 4], [7, 3, 7, 6, 5, 5],
-		[7, 3, 7, 6, 5, 6], [7, 3, 7, 6, 5, 7], [7, 3, 7, 6, 6, 0],
-		[7, 3, 7, 6, 6, 1], [7, 7, 7, 7, 6, 5], [7, 7, 7, 7, 6, 6],
-		[1, 5, 7, 7, 6, 7], [1, 2, 3, 2, 3, 0], [3, 3, 3, 3, 3, 1],
-		[1, 1, 2, 0, 0, 2], [3, 3, 7, 7, 7, 3], [7, 7, 7, 7, 7, 4],
-		[7, 7, 7, 7, 7, 5], [7, 7, 7, 7, 7, 6], [7, 7, 7, 7, 7, 7]] }
-	let (:limits_arr) { [4, 4, 4, 4, 4 ,4] }
+  let (:permutation) { [[7, 3, 7, 6, 5, 4], [7, 3, 7, 6, 5, 5],
+    [7, 3, 7, 6, 5, 6], [7, 3, 7, 6, 5, 7], [7, 3, 7, 6, 6, 0],
+    [7, 3, 7, 6, 6, 1], [7, 7, 7, 7, 6, 5], [7, 7, 7, 7, 6, 6],
+    [1, 5, 7, 7, 6, 7], [1, 2, 3, 2, 3, 0], [3, 3, 3, 3, 3, 1],
+    [1, 1, 2, 0, 0, 2], [3, 3, 7, 7, 7, 3], [7, 7, 7, 7, 7, 4],
+    [7, 7, 7, 7, 7, 5], [7, 7, 7, 7, 7, 6], [7, 7, 7, 7, 7, 7]] }
+  let (:limits_arr) { [4, 4, 4, 4, 4 ,4] }
 
-	it "reject_each_more_than(limits_arr)" do
-		result = permutation.reject_each_more_than(limits_arr)
-		expect(result).to eq [[1, 2, 3, 2, 3, 0], [3, 3, 3, 3, 3, 1], [1, 1, 2, 0, 0, 2]]
-	end
+  it "reject_each_more_than(limits_arr)" do
+    result = permutation.reject_each_more_than(limits_arr)
+    expect(result).to eq [[1, 2, 3, 2, 3, 0], [3, 3, 3, 3, 3, 1], [1, 1, 2, 0, 0, 2]]
+  end
 
-	it "removes values from array taht are bigger than 7" do
-		result = permutation[0].reject_expensive(7)
-		expect(result).to eq [3, 6, 5, 4]
-	end
+  it "removes values from array taht are bigger than 7" do
+    result = permutation[0].reject_expensive(7)
+	    expect(result).to eq [3, 6, 5, 4]
+  end
 
-	it "removes values from array taht are bigger than 3" do
-		result = permutation[9].reject_expensive(3)
-		expect(result).to eq [1, 2, 2, 0]
-	end
+  it "removes values from array taht are bigger than 3" do
+    result = permutation[9].reject_expensive(3)
+    expect(result).to eq [1, 2, 2, 0]
+  end
 
-	it "removes values from array taht are bigger 6" do
-		result = permutation[5].reject_expensive(6)
-		expect(result).to eq [3, 1]
-	end
+  it "removes values from array taht are bigger 6" do
+    result = permutation[5].reject_expensive(6)
+    expect(result).to eq [3, 1]
+  end
 
-	it "removes values from array taht are bigger than a given number" do
-		result = [580, 355, 335, 275, 215].reject_expensive(1720)
-		expect(result).to eq [580, 355, 335, 275, 215]
-	end		
+  it "removes values from array taht are bigger than a given number" do
+    result = [580, 355, 335, 275, 215].reject_expensive(1720)
+    expect(result).to eq [580, 355, 335, 275, 215]
+  end
+
+  it "converts integers array to array of strings as decimals with precision 2" do
+    result = [2, 4, 6, 8].to_money
+    expect(result).to eq ["0.02", "0.04", "0.06", "0.08"]
+  end
+
+  it "select_nonzero_indexes" do
+    result = [1, 0, 2, 0, 0, 1].select_nonzero_indexes
+    expect(result).to eq [0, 2, 5]
+  end
+
+  it "select_result_prices(prices)" do
+    prices = ["0.02", "0.04", "0.06", "0.08"]
+    result = [0, 2, 5].select_result_prices(prices)
+    expect(result).to eq ["0.02", "0.06"]
+  end
+
+  it "combine arrays quantity with corresponding selected prices" do
+    selected_prices = ["0.02", "0.06"]
+    result = [1, 2, 1].combine(selected_prices)
+    expect(result).to eq [[1, "0.02"], [2, "0.06"]]
+  end
+
+  it "combine_with(menu, prices)" do #siwka zmienic opisy "it"
+    menu = {"french fries"=>"2.75", "side salad"=>"3.35", "hot wings"=>"3.55", "mozzarella"=>"4.20", "sampler plate"=>"5.80", "sss"=>"4.20"}
+    prices = ["2.75", "4.20"]
+    result = [1, 2].combine_with(menu, prices)
+  	expect(result).to eq [[1, ["french fries"], "2.75"], [2, ["mozzarella", "sss"], "4.20"]]
+  end
+
+  it "removes last elements from longer array to make their length even" do
+    selected_prices = ["2.02", "3.06", "5.02", "0.06"]
+    result = [1, 2, 3].validate_eq_length(selected_prices)
+    expect(result).to eq [1, 2, 3]
+  end
+
+  it "match_prices_with_entrees(menu, prices)" do
+  	results = [[0, 0, 0, 0, 0, 7]]
+    menu = {"expensive plate"=>"15.80", "mixed fruit"=>"2.15", "french fries"=>"2.75", "side salad"=>"3.35", "hot wings"=>"3.55", "mozzarella sticks"=>"4.20", "sampler plate"=>"5.80"}
+    prices = ["5.80", "4.20", "3.55", "3.35", "2.75", "2.15"]
+    result = results.match_prices_with_entrees(menu, prices)
+    expect(result).to eq [[7, ["mixed fruit"], "2.15"]]
+  end    
+
+  it "match_prices_with_entrees(menu, prices)" do
+  	results = [[0, 0, 0, 0, 0, 7], [1, 0, 2, 0, 0, 1]]
+    menu = {"expensive plate"=>"15.80", "mixed fruit"=>"2.15", "french fries"=>"2.75", "side salad"=>"3.55", "hot wings"=>"3.55", "mozzarella sticks"=>"4.20", "sampler plate"=>"5.80"}
+    prices = ["5.80", "4.20", "3.55", "3.35", "2.75", "2.15"]
+    result = results.match_prices_with_entrees(menu, prices)
+    expect(result).to eq [[7, ["mixed fruit"], "2.15"], [1, ["sampler plate"], "5.80"], [2, ["side salad", "hot wings"], "3.55"], [1, ["mixed fruit"], "2.15"]]
+  end   
 end
